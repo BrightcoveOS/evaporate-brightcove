3b48523b831b952501ed8f653b5237060726305d
Bryce Fisher-Fleig <brycefisher@users.noreply.github.com>
2017-04-30 13:44:17 -0700

    (refactor) Shrink UMD bundle from ~1.4mb to ~350kb (#8)
    * (refactor) simply crypto helper module
    
    Removing unused code paths to simplify and further refactor with the
    ultimate aim of removing an extra large dependency.
    
    * (nit) Code style in crypto helper
    
    * (feat) Remove AWS-SDK from dependencies

--- src/crypto.js
+++ src/crypto.js
@@ -12,18 +12,12 @@ function sha256(data) {
   return _hash('sha256', data, 'hex');
 }
 
-function arraySliceFn(obj) {
-  var fn = obj.slice || obj.webkitSlice || obj.mozSlice;
-  return typeof fn === 'function' ? fn : null;
-}
-
-function _hash(algorithm, data, digest, callback) {
+function _hash(algorithm, data, digest) {
   var hash = crypto.createHash(algorithm);
 
-  if (!digest) { digest = 'binary'; }
-  if (digest === 'buffer') { digest = undefined; }
-  if (typeof data === 'string') { data = new Buffer(data); }
-  var sliceFn = arraySliceFn(data);
+  if (typeof data === 'string') {
+    data = new Buffer(data);
+  }
   var isBuffer = Buffer.isBuffer(data);
 
   //Identifying objects with an ArrayBuffer as buffers
@@ -31,46 +25,11 @@ function _hash(algorithm, data, digest, callback) {
     isBuffer = true;
   }
 
-  if (callback && typeof data === 'object' && typeof data.on === 'function' && !isBuffer) {
-    data.on('data', function(chunk) { hash.update(chunk); });
-    data.on('error', function(err) { callback(err); });
-    data.on('end', function() { callback(null, hash.digest(digest)); });
-
-  } else if (callback && sliceFn && !isBuffer && typeof FileReader !== 'undefined') {
-    // this might be a File/Blob
-    var index = 0, size = 1024 * 512;
-    var reader = new FileReader();
-
-    reader.onerror = function() {
-      callback(new Error('Failed to read data.'));
-    };
-
-    reader.onload = function() {
-      var buf = new Buffer(new Uint8Array(reader.result));
-      hash.update(buf);
-      index += buf.length;
-      reader._continueReading();
-    };
-
-    reader._continueReading = function() {
-      if (index >= data.size) {
-        return callback(null, hash.digest(digest));
-      }
-
-      var back = index + size;
-      if (back > data.size) { back = data.size; }
-      reader.readAsArrayBuffer(sliceFn.call(data, index, back));
-    };
-
-    reader._continueReading();
-  } else {
-    if (typeof data === 'object' && !isBuffer) {
-      data = new Buffer(new Uint8Array(data));
-    }
-    var out = hash.update(data).digest(digest);
-    if (callback) { callback(null, out); }
-    return out;
+  if (typeof data === 'object' && !isBuffer) {
+    data = new Buffer(new Uint8Array(data));
   }
+
+  return hash.update(data).digest(digest);
 }
 
 module.exports = {
